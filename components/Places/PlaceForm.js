8eba5c6afddc16a820a2dd266da750fe8b18b236
Cüneyt Ekrem ARI <cuneyt7088@gmail.com>
2023-05-09 12:05:58 +0300

    project structure done

---
+++ components/Places/PlaceForm.js
@@ -0,0 +1,13 @@
+import { Text } from "react-native"
+import { View } from "react-native"
+
+
+const PlaceForm = () => {
+  return (
+    <View>
+        <Text>The Place Form</Text>
+    </View>
+  )
+}
+
+export default PlaceForm
